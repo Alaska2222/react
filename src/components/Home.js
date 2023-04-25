56129d3989510dc817a75e8ffde8044264bd8d73
Alaska <sindrella975@gmail.com>
2023-04-25 18:59:07 +0300

    Get METHOD(teachers list)

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,26 +10,26 @@ export default function Home(){
      }, []);
 
      return (
-        <body>
-            <div className="info">
+        <div>
+            <main>
                 <h1><span className="highlight">GradeMaster</span></h1>
                 <h3>Welcome to GradeMaster, the ultimate website for managing and rating your academic progress!</h3>
                 <p>
                     For students we offer convinient system for monitoring grades for each course and assignment.
                     As for teachers, we developed a rapid evaluation scheme.
                 </p>
-            </div>
+            </main>
             <div className="image-block">
-                <div className="member">
+                <div className="image">
                     <img src= {hist1} alt="first histogram" />
                 </div>
-                <div className="member">
+                <div className="image">
                     <img src={hist2} alt="second histogram" />
                 </div>
-                <div className="member">
+                <div className="image">
                     <img src={hist3} alt="third histogram" />
                 </div>
             </div>
-        </body>
+        </div>
     )
 }
--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,8 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
+import Teacher from './Teacher';
+import "../styles/staff.css"
+
 
 export default function Staff(){
+    let [records, setRecord] = useState([])
     useEffect(() => {
         document.title = "Our Staff"
-     }, []);
-    return <h1> Staff page</h1>
+        fetch("http://127.0.0.1:5000/teachers")
+    .then((response)=>{
+         return response.json(); 
+     })
+    .then((data)=>{
+        setRecord(data)
+     })
+    .catch(err => console.log(err))
+     }, [])
+
+    
+    
+
+    return (
+     <div>
+        <h1><span className="highlight">GradeMaster`s Teaching Staff</span></h1>
+        <div className="teacher-list">
+            <Teacher Username="petroPu" Name="Petro" Surname="Pukach" Age="50" Phone="+380958984251" Email="PetroPukacg@gmail.com"/>
+            {records.map((obj) => (
+                <Teacher key={obj.TeacherId} Username= {obj.TeacherId} Name={obj.Firstname} Surname={obj.Surname}
+                 Age={obj.Age} Subject={obj.SubjectId} Phone={obj.Phone} Email={obj.Email} />
+            ))}
+        </div>
+    </div>
+    );
 }
