370307bcdd47cecafc3354f912d5698e3a64e2f2
Alaska <sindrella975@gmail.com>
2023-04-28 15:15:16 +0300

    Protected Routes + Login

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ import Login from "./pages/Login"
 import Profile from "./pages/Profile"
 import Staff from "./pages/Staff"
 import Register from "./pages/Register"
-import {Route, Routes} from "react-router-dom"
+import {Route, Routes, Navigate} from "react-router-dom"
 import "./styles/main.css"
 
 function App() {
+
+  const ProtectedRoute = ({ allowedRoles, children }) => {
+    const userRole = localStorage.getItem('role');
+    if (!allowedRoles.includes(userRole)) {
+      return <Navigate to="/" replace />;
+    }
+    return children;
+  };
+
   return (
   <>
+      
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute allowedRoles={["user"]}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/staff" element={<Staff />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<h1>Admin page</h1>} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={["admin"]}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<h1>There's nothing here: 404!</h1>} />
       </Routes>
    </>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,42 @@
 import React, { useEffect,useState} from 'react';
 import Button from '../components/Button';
 import "../styles/login.css"
-import { Link, json } from 'react-router-dom';
-
-const LOGIN_URL = "/login"
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login(){
     useEffect(() => {
         document.title = "Login"
      }, []);
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await fetch("http://127.0.0.1:5000/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+          },
+        });
+        if (response.ok) { 
+            const data = await response.json();
+            console.log(data.Username, data.Status);
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+            localStorage.setItem('role', data.Status);
+            navigate('/')
+            console.log(localStorage.getItem('username'), localStorage.getItem('password'))
+        } 
+      };
+
     return(
         <>
             <div className="form-block">
             
                 <div className="side-image"></div>
-                <form method="#" className="log-in"> 
+                <form method="#" className="log-in" onSubmit={handleSubmit}> 
                     <h1><span className="highlight">Log in</span></h1>
                     <div className="floating-label">
                         <input className="input"
@@ -23,6 +44,8 @@ export default function Login(){
                          type="text"
                          name="username"
                          id="username"
+                         value={username}
+                         onChange={(e) => setUsername(e.target.value)}
                          required/>
                         <label htmlFor="username">Username:</label>
                     </div>
@@ -32,6 +55,8 @@ export default function Login(){
                          type="password"
                          name="password"
                          id="password"
+                         value={password}
+                         onChange={(e) => setPassword(e.target.value)}
                          required/>
                         <label htmlFor="password">Password:</label>
                     </div>
