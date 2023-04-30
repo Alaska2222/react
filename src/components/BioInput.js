5bfe3460e31af6188b1fb9e8a1a59084cda94a42
Alaska <sindrella975@gmail.com>
2023-04-30 16:27:53 +0300

    User Profile Get data

--- src/components/BioInput.js
+++ src/components/BioInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BioInput({Label, Name, Type, Id }){
     return (
-        <div class="bio-input">
+        <div className="bio-input">
             <label htmlFor={Id}>{Label}:</label>
             <input className="input" placeholder={Name} type={Type} id={Id} disabled />
         </div>)       
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,27 +1,88 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import BioInput from '../components/BioInput';
 import TableItem from '../components/TableItem';
 import Button from '../components/Button';
 import profile from "../assets/profile.png"
 
 export default function Profile(){
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [age, setAge] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [group, setGroup] = useState('')
+
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+
+    let [records, setRecord] = useState([])
     useEffect(() => {
-        document.title = "User Profile"
-     }, []);
+        const fetchData = () => {
+            try {
+                fetch(`http://127.0.0.1:5000/student/${username}`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa(username + ':' + password)
+                    }
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    const { Firstname, Surname, Age, Email, Phone, GroupId } = data.Student;
+    
+                    setName(Firstname);
+                    setSurname(Surname);
+                    setAge(Age);
+                    setEmail(Email);
+                    setPhone(Phone);
+                    setGroup(GroupId);
+    
+                    document.title = "User Profile";
+    
+                    return fetch(`http://127.0.0.1:5000/students/${username}`, {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Basic ' + btoa(username + ':' + password)
+                        }
+                    });
+                })
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    setRecord(data);
+                    console.log(records)
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+    
+
     return (
         <>
             <h1 id="profile-title"><span className="highlight">Welcome to the, student profile!</span></h1>
                 <div className="wrapper"> 
                     <div className="bio-block">
                     <img src={profile} alt="profile logo"/>
-                    <h3>Alaska11</h3>
+                    <h3>{username}</h3>
                     <div className="inputs-block">
-                        <BioInput Label="Name" Name="Kornelia" Type="text" Id="name"/>
-                        <BioInput Label="Surname" Name="Drozd" Type="text" Id="surname"/>
-                        <BioInput Label="Group" Name="216" Type="number" Id="group"/>
-                        <BioInput Label="Age" Name="18" Type="number" Id="age"/>
-                        <BioInput Label="Phone" Name="+380958984251" Type="tel" Id="phone"/>
-                        <BioInput Label="Email" Name="sindrella975@gmail.com" Type="email" Id="email"/>
+                        <BioInput Label="Name" Name={name} Type="text" Id="name"/>
+                        <BioInput Label="Surname" Name={surname} Type="text" Id="surname"/>
+                        <BioInput Label="Group" Name={group} Type="number" Id="group"/>
+                        <BioInput Label="Age" Name={age} Type="number" Id="age"/>
+                        <BioInput Label="Phone" Name={phone} Type="tel" Id="phone"/>
+                        <BioInput Label="Email" Name={email} Type="email" Id="email"/>
                     </div>
                     <div className ="button-group">
                         <Button Id="update-btn" Title="Update"/>
@@ -41,16 +102,10 @@ export default function Profile(){
                             </tr>
                             </thead>
                             <tbody>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>  
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
-                                <TableItem Id="1" Sbj="Theory of Information" Teacher="petroPu" Value="5" Date="25/08/2004"/>
+                            {records.map((obj) => (
+                                <TableItem key={obj.MarkId} Id={obj.MarkId} Sbj={obj.SubjectId} Teacher={obj.TeacherId} 
+                                Value={obj.Value} Date={obj.DateId} />
+                            ))}
                             </tbody>
                             </table>
                         </div>
